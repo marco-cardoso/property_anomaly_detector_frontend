ad74b6b796603515ccbc3053b5b4a5dcda31cd44
Marco Cardoso <marcom.c@outlook.com>
2020-02-08 20:57:23 -0400

    Feature engineering

--- utils.py
+++ utils.py
@@ -8,15 +8,17 @@ class Database():
     def __init__(self):
         self.client = MongoClient(database_host, database_port)
         self.database = self.client[database_name]
-        
         self.properties = self.database['properties']
         
     def get_properties(self):
         return list(self.properties.find({},
                                          {
+                                             'title' : 1,
+                                             'letting.Furnishing:' : 1,
                                              'price' : 1, 
                                              'latitude' : 1,
-                                             'longitude' : 1
+                                             'longitude' : 1,
+                                             'url' : 1
                                          }
                                         ))
           
