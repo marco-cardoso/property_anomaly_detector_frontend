2f749f39ad25a99ad1120fc4e7ded01a9a077b10
Marco Cardoso <marcom.c@outlook.com>
2020-06-20 10:13:41 +0100

    Update EDA notebook

--- src/property_anomaly_detector/features/read_df.py
+++ src/property_anomaly_detector/features/read_df.py
@@ -9,6 +9,14 @@ def read_df():
     properties = database.get_properties(
         default_projection={
             '_id': -1,
+            'rental_prices.shared_occupancy' : 1,
+            'num_floors' : 1,
+            'num_bedrooms' : 1,
+            'num_bathrooms' : 1,
+            'furnished_state' : 1,
+            'category' : 1,
+            'property_type' : 1,
+            'num_recepts' : 1,
             'latitude': 1,
             'longitude': 1,
             'rental_prices.per_month': 1,
@@ -17,8 +25,25 @@ def read_df():
         }
     )
 
-    df = pd.DataFrame(properties, columns=['latitude', 'longitude', 'rental_prices', 'outcode'])
-    df['rental_prices'] = pd.to_numeric(df['rental_prices'].apply(lambda x: x['per_month']))
+    df = pd.DataFrame(properties)
+    del df['_id']
+
+    df['monthly_rental_price'] = df['rental_prices'].apply(lambda x: x['per_month'])
+    df['shared_occupancy'] = df['rental_prices'].apply(lambda x: x['shared_occupancy'])
+
+    del df['rental_prices']
+
+    numerical_columns = [
+        'monthly_rental_price',
+        'num_floors',
+        'num_bedrooms',
+        'num_bathrooms',
+        'num_recepts',
+        'latitude',
+        'longitude'
+    ]
+
+    df[numerical_columns] = df[numerical_columns].astype(float)
 
     df.rename(columns={
         'rental_prices': 'monthly_rental_price',
