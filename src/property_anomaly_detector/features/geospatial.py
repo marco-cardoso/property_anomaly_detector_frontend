a8dba75fcf6c3375a4d8ac51f414e995f227b807
Marco Cardoso <marcom.c@outlook.com>
2020-07-06 16:10:01 +0100

    Geospatial plots

--- src/property_anomaly_detector/features/geospatial.py
+++ src/property_anomaly_detector/features/geospatial.py
@@ -1,15 +1,16 @@
 from os import path
 
+import geopandas as gpd
 import numpy as np
 import pandas as pd
-import geopandas as gpd
-
-import matplotlib.pyplot as plt
-
 import property_anomaly_detector
 
 
 def get_london_geojson() -> gpd.GeoDataFrame:
+    """
+    It loads the geojson with the london districts
+    :return: A geopandas dataframe with the london districts
+    """
     root_path = path.dirname(property_anomaly_detector.__file__)
     geojson_path = path.join(root_path, "datasets", "london.geojson")
     london = gpd.read_file(geojson_path)
@@ -17,50 +18,70 @@ def get_london_geojson() -> gpd.GeoDataFrame:
 
 
 def get_properties_gdf(df: pd.DataFrame) -> gpd.GeoDataFrame:
+    """
+    It converts a pandas dataframe with latitude and longitude into a geopandas dataframe
+    :param df: A pandas dataframe with at least three columns : 'latitude', 'longitude' and 'monthly_rental_price'
+    :return: A geopandas dataframe
+    """
     properties_gpd = gpd.GeoDataFrame(
         df[['latitude', 'longitude', 'monthly_rental_price']],
         geometry=gpd.points_from_xy(df['longitude'], df['latitude'])
     )
-
+    properties_gpd.crs = {'init': 'epsg:4326'}
     return properties_gpd
 
 
 def plot_london_layer():
+    """
+    It plots the london districts.
+    :return: The plot axis
+    """
     ax = london.plot()
     ax.set_axis_off()
     return ax
 
 
-def plot_properties_layer(df: pd.DataFrame, axis):
-    properties_gpd = get_properties_gdf(df)
-    properties_gpd.plot(ax=axis, color='red', marker=1)
-    plt.show()
+def plot_geospatial_layer(df: pd.DataFrame, func=np.median, orientation="vertical", title=None, **kwargs):
+    """
 
+    It uses geopandas to plot a geospatial layer
 
-def plot_median_district_layer(df: pd.DataFrame):
+    :param df: A pandas dataframe with at least three columns : latitude, longitude and monthly_rental_price
+    :param func: A numpy function to aggregate the pandas dataframe observations
+    :param orientation: The legend orientation. Options available : 'horizontal' and 'vertical'
+    :param title: The plot title
+    :param kwargs: Arguments to customize the matplotlib plot
+    :return: A dataframe with the aggregation result
+    """
     properties_gdf = get_properties_gdf(df)
 
     join = gpd.sjoin(london, properties_gdf, how="inner")
 
-    district_median_price = join.groupby(['name'], as_index=False).agg(
-        {'monthly_rental_price': np.median, 'geometry': 'first'})
+    district_median_price_agg = join.groupby(['name'], as_index=False).agg(
+        {'monthly_rental_price': func, 'geometry': 'first'})
 
     district_median_price = gpd.GeoDataFrame(
-        district_median_price[['name', 'monthly_rental_price']],
-        geometry=district_median_price['geometry']
+        district_median_price_agg[['name', 'monthly_rental_price']],
+        geometry=district_median_price_agg['geometry']
     )
 
     # ax = district_median_price.plot( alpha=0.5, edgecolor='k')
     ax = district_median_price.plot(
-        "monthly_rental_price", figsize=(20, 7), alpha=0.8,
+        "monthly_rental_price",
+        alpha=0.8,
         edgecolor='k',
         legend=True,
         legend_kwds={
-            "label": "Monthly median rental price"
+            "label": "Monthly median rental price",
+            "orientation": orientation
         },
-        cmap="plasma"
+        cmap="plasma",
+        **kwargs
     )
+
+    ax.set_title(title)
     ax.set_axis_off()
+    return district_median_price_agg
 
 
 london = get_london_geojson()
