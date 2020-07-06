75c01547bae981958dc2f102fd7912400abb51f4
Marco Cardoso <marcom.c@outlook.com>
2020-07-06 17:40:23 +0100

    Document OD method

--- src/property_anomaly_detector/ml/detect_anomalies.py
+++ src/property_anomaly_detector/ml/detect_anomalies.py
@@ -1,9 +1,21 @@
 import numpy as np
+import pandas as pd
 from property_anomaly_detector.features import read_df
 from pyod.models.knn import KNN
 
 
-def detect(contamination=0.03, n_neighbors=50):
+def detect(contamination: float = 0.03, n_neighbors: int = 50) -> pd.DataFrame:
+    """
+    It detects the outlier properties over the database using KNN.
+
+    To get the default contamination and n_neighbors method values a simple
+    experiment was conducted. 50 outliers were labelled and a manual grid search
+    executed.
+
+    :param contamination: KNN contamination value
+    :param n_neighbors: KNN n_neighbors value
+    :return: A pandas dataframe with the anomalies
+    """
     df = read_df()
 
     df['shared_occupancy'] = df['shared_occupancy'].map({'Y': 1, 'N': 0})
