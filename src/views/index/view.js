37a86739d3083d85be2f8e0b13c00ecb3f2a33a5
Marco Cardoso <marcom.c@outlook.com>
2020-07-16 15:22:25 +0100

    Update react structure

---
+++ src/views/index/view.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Styles
+import {useStyles} from './style'
+
+
+export default function Index() {
+    const classes = useStyles();
+
+    return (
+      <h1>Hello </h1>
+    );
+}
