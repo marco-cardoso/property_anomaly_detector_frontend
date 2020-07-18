7a5dbf81c8500b9d4d9d1f17c1b99d733e537b8f
Marco Cardoso <marcom.c@outlook.com>
2020-07-18 17:43:57 +0100

    Anomalies filter component

---
+++ src/components/forms/AnomaliesForm.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Grid from "@material-ui/core/Grid"
+import Slider from '@material-ui/core/Slider';
+
+function valuetext(value) {
+    return `${value}°C`;
+  }
+
+export default function AnomaliesForm({filters, setFilters}){
+
+    const [value, setValue] = React.useState([0, 20]);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <>
+            <h3>Filters</h3>
+            <Grid container xs={12}>
+                <Slider
+                    value={value}
+                    onChange={handleChange}
+                    valueLabelDisplay="on"
+                    aria-labelledby="range-slider"
+                    getAriaValueText={valuetext}
+                    min={0}
+                    max={20}
+                />
+                
+            </Grid>
+
+        </>
+    )
+}
