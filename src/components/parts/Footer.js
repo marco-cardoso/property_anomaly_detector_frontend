74e74049c225f3e3293c5dbd1cdfff07a018273d
Marco Cardoso <marcom.c@outlook.com>
2020-07-24 20:04:03 +0100

    Zoopla image user requirement

--- src/components/parts/Footer.js
+++ src/components/parts/Footer.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <h1>  </h1>
+       <>
+        <img src="https://www.zoopla.co.uk/static/images/mashery/powered-by-zoopla-150x73.png" 
+            width="150" 
+            height="73" 
+            title="Property information powered by Zoopla" 
+            alt="Property information powered by Zoopla" 
+            border="0"
+            style={{bottom : 20 , right : 50, position : 'absolute'}}
+            >
+        </img>
+       </>
     )
 }
